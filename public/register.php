dcbe276a4a14ae226cca073242153b8d6a520822
Cheneba <chenebametuge14@gmail.com>
2025-03-13 00:43:38 +0100

    Added the functionality needed to effectively register a new user

--- public/register.php
+++ public/register.php
@@ -1,10 +1,14 @@
 <?php include "../includes/header.php" ?>
+<?php include "../includes/functions.php" ?>
 
+<?php
+echo $msg ?: null;
+?>
 
 <div class="container">
   <div class="shadow mx-auto pb-3" style="width: 350px; margin-bottom: 140px; margin-top: 140px">
     <div class="px-5 py-2">
-      <form action="login.html">
+      <form action="" method="post">
         <br />
         <br />
         <!-- Name and Email input -->
@@ -20,7 +24,8 @@
         <br />
         <br />
         <!-- Register button -->
-        <a href="" class="btn btn-primary btn-lg" style="margin-left: 80px">Register</a>
+        <button type="submit" name="submit-register" class="btn btn-primary btn-lg"
+          style="margin-left: 80px">Register</button>
       </form>
     </div>
   </div>
