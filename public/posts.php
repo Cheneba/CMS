04abc3ad5308d922ce916e1feb865f21d43b23bd
Cheneba <chenebametuge14@gmail.com>
2025-03-12 13:26:22 +0100

    Added the conditionals for if a new post is to be registered or if a post is to be edited and added the necessary bootstrap attributes needed for the functioning of the modals and included a page to hold the modal div and overall made use of modals for their functioning

--- public/posts.php
+++ public/posts.php
@@ -6,49 +6,84 @@ $posts = getAllPosts();
 
 if (isset($_POST['delete'])) {
   $d = explode(',', base64_decode($_POST['delete']));
-  var_dump(deletePostWithUserId(...$d));
-  header("Location: /CMS/public/posts.php");
+  if (deletePostWithUserId(...$d)) {
+    header("Location: /CMS/public/posts.php");
+  }
 }
-?>
 
+if (isset($_POST["submit-newmodal"])) {
+  $title = htmlspecialchars($_POST['title']);
+  $description = htmlspecialchars($_POST['description']);
+  $content = htmlspecialchars($_POST['content']);
+  $user_id = $user_id ?: 2;
+
+  if ($title && $description && $user_id) {
+    if (newPostWithUserId($user_id, $title, $description, $content)) {
+      header("Location: /CMS/public/posts.php");
+    }
+  }
+}
+
+
+if (isset($_POST["submit-editmodal"])) {
+  $title = htmlspecialchars($_POST['title']);
+  $description = htmlspecialchars($_POST['description']);
+  $content = htmlspecialchars($_POST['content']);
+  $user_id = $user_id ?: 2;
+
+  if ($title && $description && $user_id) {
+    if (newPostWithUserId($user_id, $title, $description, $content)) {
+      $msg = "Successful";
+    }
+  }
+}
+
+?>
 <div class="container">
   <div class="row">
     <div class="col-12 mb-5">
       <div class="row">
-        <div class="col offset-8"></div>
+        <div class="col-8"></div>
         <div class="col-3">
-          <a href="#" class="btn btn-outline-success" style="margin-left: 10px">New Post</a>
+          <button class="btn btn-outline-success" type="button" class="btn btn-primary" data-bs-toggle="modal"
+            data-bs-target="#myModal1">
+            New Post
+          </button>
         </div>
       </div>
     </div>
-
     <?php $counter = 0 ?>
     <?php foreach ($posts['posts'] as $post): ?>
 
-      <!-- Single Post Description -->
-      <div class="col-12 shadow">
-        <div class="py-3 pl-3">
-          <h3><?php echo $post[1] ?></h3>
-          <h6 class="text-decoration-underline">Description</h6>
-          <p>
-            <?php echo $post[3] ?>
-          </p>
+    <!-- Single Post Description -->
+    <div class="col-12 shadow">
+      <div class="py-3 pl-3">
+        <h3><?php echo $post[1] ?></h3>
+        <h6 class="text-decoration-underline">Description</h6>
+        <p>
+          <?php echo $post[3] ?>
+        </p>
 
-          <div class="row">
-            <div class="col-7">
-              <span> Author: <?php echo $posts['authors'][$counter];
+        <div class="row">
+          <div class="col-7">
+            <span> Author: <?php echo $posts['authors'][$counter];
                               $counter++; ?> </span>
-            </div>
-            <form method="post" action="posts.php" class="col-5">
-              <button href="#" class="btn btn-success">Edit</button>
-              <button href="#" class="btn btn-danger" name="delete"
+          </div>
+          <div class="col-5 d-flex">
+
+            <button class="btn btn-success wl-2" type="button" class="btn btn-primary" data-bs-toggle="modal"
+              data-bs-target="#myModal2">
+              Edit</button>
+            <form method="post" action="posts.php">
+              <button class="btn btn-danger" name="delete"
                 value="<?php echo base64_encode($post[0] . "," . $post[5]) ?>">Delete</button>
-              <a href="post.php?d=<?php echo base64_encode($post[0]) ?>" class="btn btn-primary">View More</a>
             </form>
+            <a href="post.php?d=<?php echo base64_encode($post[0]); ?>" class="btn btn-primary">View More</a>
           </div>
         </div>
       </div>
-      <!-- Single Post Description -->
+    </div>
+    <!-- Single Post Description -->
 
     <?php endforeach; ?>
 
@@ -60,6 +95,8 @@ if (isset($_POST['delete'])) {
     <!-- View more -->
   </div>
 </div>
+<?php include "modal.php" ?>
+
 
 
 <?php include "../includes/footer.php" ?>
