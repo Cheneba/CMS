dd2c26dc8bc32f88b5b87eca017244101ab369e8
Cheneba <chenebametuge14@gmail.com>
2025-03-13 02:22:08 +0100

    Made it dynamic and reactive

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,5 +1,11 @@
 <?php include "../includes/header.php" ?>
-
+<?php include "../includes/functions.php" ?>
+<?php
+$posts = getAllPosts();
+$user_posts = getPostsWithUserId($_SESSION["user_id"]);
+$other_posts = getPostsWithoutUserId($_SESSION["user_id"]);
+var_dump($_SERVER["PHP_SELF"]);
+?>
 
 
 <div class="container">
@@ -8,7 +14,9 @@
       <h1>Hello, User</h1>
     </div>
     <div class="col-3">
-      <a href="#" class="btn btn-outline-success" style="margin-left: 10px">New Post</a>
+      <button class="btn btn-outline-success" type="button" data-bs-toggle="modal" data-bs-target="#myModal1">
+        New Post
+      </button>
     </div>
   </div>
   <br />
@@ -16,50 +24,43 @@
   <br />
 
   <div class="row">
-    <!-- Single Post Description -->
-    <div class="col-12 shadow">
-      <div class="py-3 pl-3">
-        <h3>Post Title</h3>
-        <h6 class="text-decoration-underline">Description</h6>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
-          beatae?
-        </p>
 
-        <div class="row">
-          <div class="col-9">
-            <span> Author: Theophilus Mbu </span>
-          </div>
-          <div class="col-3">
-            <a href="#" class="btn btn-success">Edit</a>
-            <a href="#" class="btn btn-danger">Delete</a>
+    <?php $counter = 0 ?>
+    <?php foreach ($user_posts as $post): ?>
+      <?php if ($counter == 3) break; ?>
+
+      <!-- Single Post Description -->
+      <div class="col-12 shadow">
+        <div class="py-3 pl-3">
+          <h3><?php echo $post[1] ?></h3>
+          <h6 class="text-decoration-underline">Description</h6>
+          <p>
+            <?php echo $post[3] ?>
+          </p>
+
+          <div class="row">
+            <div class="col-7">
+              <span> Author: By me </span>
+            </div>
+            <div class="col-5 d-flex">
+
+              <button class="btn btn-success wl-2" type="button" class="btn btn-primary" data-bs-toggle="modal"
+                data-bs-target="#myModal2">
+                Edit</button>
+              <form method="post" action="dashboard.php">
+                <button class="btn btn-danger" name="delete"
+                  value="<?php echo base64_encode($post[0] . "," . $post[5]) ?>">Delete</button>
+              </form>
+              <a href="post.php?d=<?php echo base64_encode($post[0]); ?>" class="btn btn-primary">View More</a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <!-- Single Post Description -->
-    <!-- Single Post Description -->
-    <div class="col-12 shadow">
-      <div class="py-3 pl-3">
-        <h3>Post Title</h3>
-        <h6 class="text-decoration-underline">Description</h6>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
-          beatae?
-        </p>
+      <!-- Single Post Description -->
 
-        <div class="row">
-          <div class="col-9">
-            <span> Author: Theophilus Mbu </span>
-          </div>
-          <div class="col-3">
-            <a href="#" class="btn btn-success">Edit</a>
-            <a href="#" class="btn btn-danger">Delete</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Single Post Description -->
+    <?php endforeach; ?>
+
+
 
     <!-- View more -->
     <div class="col-12 text-center mt-5" style="height: 50px">
@@ -72,50 +73,45 @@
   <br />
 
   <div class="row">
-    <!-- Single Post Description -->
-    <div class="col-12 shadow">
-      <div class="py-3 pl-3">
-        <h3>Post Title</h3>
-        <h6 class="text-decoration-underline">Description</h6>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
-          beatae?
-        </p>
 
-        <div class="row">
-          <div class="col-9">
-            <span> Author: Theophilus Mbu </span>
-          </div>
-          <div class="col-3">
-            <a href="#" class="btn btn-success">Edit</a>
-            <a href="#" class="btn btn-danger">Delete</a>
+
+    <?php $counter = 0 ?>
+    <?php foreach ($other_posts['posts'] as $post): ?>
+      <?php if ($counter == 3) break; ?>
+
+      <!-- Single Post Description -->
+      <div class="col-12 shadow">
+        <div class="py-3 pl-3">
+          <h3><?php echo $post[1] ?></h3>
+          <h6 class="text-decoration-underline">Description</h6>
+          <p>
+            <?php echo $post[3] ?>
+          </p>
+
+          <div class="row">
+            <div class="col-7">
+              <span> Author: <?php echo $other_posts['authors'][$counter];
+                              $counter++; ?> </span>
+            </div>
+            <div class="col-5 d-flex">
+
+              <button class="btn btn-success wl-2" type="button" class="btn btn-primary" data-bs-toggle="modal"
+                data-bs-target="#myModal2">
+                Edit</button>
+              <form method="post" action="posts.php">
+                <button class="btn btn-danger" name="delete"
+                  value="<?php echo base64_encode($post[0] . "," . $post[5]) ?>">Delete</button>
+              </form>
+              <a href="post.php?d=<?php echo base64_encode($post[0]); ?>" class="btn btn-primary">View More</a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <!-- Single Post Description -->
-    <!-- Single Post Description -->
-    <div class="col-12 shadow">
-      <div class="py-3 pl-3">
-        <h3>Post Title</h3>
-        <h6 class="text-decoration-underline">Description</h6>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
-          beatae?
-        </p>
+      <!-- Single Post Description -->
 
-        <div class="row">
-          <div class="col-9">
-            <span> Author: Theophilus Mbu </span>
-          </div>
-          <div class="col-3">
-            <a href="#" class="btn btn-success">Edit</a>
-            <a href="#" class="btn btn-danger">Delete</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Single Post Description -->
+    <?php endforeach; ?>
+
+
 
     <!-- View more -->
     <div class="col-12 text-center mt-5" style="height: 50px">
@@ -126,6 +122,7 @@
 </div>
 
 
+<?php include "modal.php" ?>
 
 
 <?php include "../includes/footer.php" ?>
